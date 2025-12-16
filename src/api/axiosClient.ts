e37cc51965ccbdda276637c969d6a9de937404b3
SotAGS <alejandrogalli8@gmail.com>
2025-12-16 13:28:28 -0300

    cambio en baseURL

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://final-mdw-backend.onrender.com/",
   // Enviar cookies (ej. cookie HttpOnly con JWT)
   withCredentials: true,
 });
